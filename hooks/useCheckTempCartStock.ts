eaaf979d78fe9ede703e200045c3d846adead439
RAREBEEF <drrobot409@gmail.com>
2023-02-07 17:00:20 +0900

    useCheckTempCartStock 에러 수정

--- hooks/useCheckTempCartStock.ts
+++ hooks/useCheckTempCartStock.ts
@@ -1,5 +1,4 @@
-import { ProductType, SizeType } from "../types";
-import { tempCartType } from "./useCartTempItem";
+import { ProductType, SizeType, TempCartType } from "../types";
 
 /**
  * 임시 카트의 제품 중 품절인 옵션이 있는지 체크, 단일 제품을 대상으로 한다.
@@ -8,7 +7,7 @@ import { tempCartType } from "./useCartTempItem";
  * @returns 재고 없을 경우 `false` 반환
  */
 const useCheckTempCartStock = () => {
-  const checkTempCartStock = (product: ProductType, tempCart: tempCartType) => {
+  const checkTempCartStock = (product: ProductType, tempCart: TempCartType) => {
     const isSoldOut: Array<boolean> = [];
 
     if (!product || !tempCart) return;
